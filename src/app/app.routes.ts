a8097bc395836f922dd5fdbfe4926a7c757bf094
Paco16889 <francisco.cobsan@gmail.com>
2025-11-08 01:41:59 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    redirectTo:'nombre',
+
+},
+{
+    path:'',
+    redirectTo:'aboutme'
+},
+{
+    path:'',
+    redirectTo:'menu'
+}];
---
+++ src/app/nombre/nombre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nombre',
+  imports: [],
+  templateUrl: './nombre.component.html',
+  styleUrl: './nombre.component.scss'
+})
+export class NombreComponent {
+
+}
