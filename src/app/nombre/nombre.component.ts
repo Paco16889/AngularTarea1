b26f92b91a8cfaa7c77222dbb73b207e7871ff53
Paco16889 <francisco.cobsan@gmail.com>
2025-11-11 18:27:01 +0100

    terminado componente Nombre

--- src/app/nombre/nombre.component.ts
+++ src/app/nombre/nombre.component.ts
@@ -1,15 +1,32 @@
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { ServicdService } from '../core/services/servicd.service';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-nombre',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './nombre.component.html',
   styleUrl: './nombre.component.scss'
 })
 export class NombreComponent {
 
+  name = '';
+  finalName = '';
+  textoMostrado = false;
+
+  
+  onEnter() {
+    if (this.name !== '') {
+      this.finalName = this.name;
+      this.textoMostrado = true;
+    }else{
+      this.textoMostrado = false;
+    }
+}
+
+ goRefresh() {
+  this.textoMostrado = false;
+  this.name = '';
+}
 
    constructor(private servicio: ServicdService){}
   goBack(){
